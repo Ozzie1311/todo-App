a689472398599f061851e9756ea12981216c57ef
Oswaldo Rodriguez <rodriosw13@gmail.com>
2025-01-01 16:18:43 -0400

    continuando con el proyecto

--- src/index.js
+++ src/index.js
@@ -4,3 +4,14 @@ import './style.css';
 
 const add = document.querySelector('#add');
 const task = new TaskList();
+const addTask = document.querySelector('#sidebar__add-task');
+const modal = document.querySelector('.modal');
+const closeModal = document.querySelector('.form__group-close');
+
+addTask.addEventListener('click', () => {
+  modal.classList.add('modal--show');
+});
+
+closeModal.addEventListener('click',() => {
+  modal.classList.remove('modal--show');
+})
