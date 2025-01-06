f42bd4c392611c89f7ac2c9d4b108c13005ebd27
Oswaldo Rodriguez <rodriosw13@gmail.com>
2025-01-06 13:19:36 -0400

    haciendo algunos cambios

--- src/index.js
+++ src/index.js
@@ -1,21 +1,3 @@
-import { TaskList } from "./taskList";
-import { CreateTask } from "./createTask";
-import { modalShow, modalClose } from "./modalShow";
-import formControl from "./formManagement";
+import "./style.css"
 
-import "./style.css";
-
-const addTaskButton = document.querySelectorAll(".add-task-button");
-const cancelButton = document.getElementById("form-cancel-button");
-
-addTaskButton.forEach((element) => {
-  element.addEventListener("click", () => {
-    modalShow();
-  });
-});
-
-cancelButton.addEventListener("click", () => {
-  modalClose();
-});
-
-formControl();
+console.log('hola mundo')
